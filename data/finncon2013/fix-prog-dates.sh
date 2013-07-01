3346dde1db7e342b47d34cec2722f2b65ede8966
Eemeli Aro <eemeli@gmail.com>
2013-07-02 01:04:28 +0300

    added readercon data, rm finncon & chicon data

--- data/finncon2013/fix-prog-dates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# in: prog.csv
-# out: prog.csv
-
-while read line; do
-	echo "$line" | sed 's!2013/07/\(..,..:..\):00!2013-07-\1!'
-done < "${1:-/proc/${$}/fd/0}"
--- data/finncon2013/json-to-js.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# in: file.json
-# out: file.js
-
-n=$(echo "$1" | sed 's/\..*//')
-echo -n "var $n = "
-cat "$1"
-echo ';'
