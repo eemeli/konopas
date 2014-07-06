0f706d1c61a29364ce0c439afc44878a835ce30d
Eemeli Aro <eemeli@gmail.com>
2014-07-06 17:37:03 +0300

    split src/prog.js from src/app.js & objectified it

--- src/app.js
+++ src/app.js
@@ -62,7 +62,7 @@ function _item_show_extra(item, id) {
 		var o = {};
 		it.tags.forEach(function(t) {
 			var cat = 'tags';
-			var tgt = _prog_hash({'tag': t});
+			var tgt = Prog.hash({'tag': t});
 			var a = t.split(':');
 			if (a.length > 1) { cat = a.shift(); t = a.join(':'); }
 			var link = '<a href="' + tgt + '">' + t + '</a>'
@@ -219,10 +219,10 @@ function item_show_list(ls, show_id) {
 
 function item_list_click(ev) {
 	function _set_location_id(id) {
-		var f = _prog_get_filters(true);
-		if (id && !f['day']) f['day'] = ko.show_all_days_by_default ? 'all_days' : _prog_default_day();
+		var f = Prog.get_filters(true);
+		if (id && !f['day']) f['day'] = ko.show_all_days_by_default ? 'all_days' : prog.default_day();
 		f['id'] = id;
-		return _prog_set_filters(f, true);
+		return Prog.set_filters(f, true);
 	}
 
 	var el = (ev || window.event).target,
@@ -436,273 +436,6 @@ function show_star_view(opt) {
 
 // ------------------------------------------------------------------------------------------------ program view
 
-function _prog_default_day() {
-	var day_start = '', day_end = '';
-	var el_dl = EL("day"); if (!el_dl) return '';
-	var dl = el_dl.getElementsByTagName("li"); if (!dl || !dl.length) return '';
-	for (var i = 0, l = dl.length; i < l; ++i) {
-		var d = dl[i].id.substr(1);
-		if (!d || d == 'll_days') continue;
-		if (!day_start || (d < day_start)) day_start = d;
-		if (!day_end || (d > day_end)) day_end = d;
-	}
-	var day_now = string_date();
-	var day = (day_now > day_start) && (day_now <= day_end) ? day_now : day_start;
-	return day;
-}
-
-
-function _prog_get_filters(hash_only) {
-	var filters = { 'day':'', 'area':'', 'tag':'', 'query':'', 'id':'' };
-	var h = window.location.toString().split('#')[1] || '';
-	var h_set = false;
-	if (h.substr(0, 4) == 'prog') {
-		var p = h.substr(5).split('/');
-		for (var i = 0; i < p.length; ++i) {
-			var s = p[i].split(':');
-			if ((s.length == 2) && s[0] && s[1]) {
-				if (ko.tag_categories) for (var j = 0; j < ko.tag_categories.length; ++j) {
-					if (s[0] == ko.tag_categories[j]) {
-						s[1] = s[0] + ':' + s[1];
-						s[0] = 'tag';
-						break;
-					}
-				}
-				filters[s[0]] = hash_decode(s[1]);
-				h_set = true;
-			}
-		}
-	}
-	if (!hash_only && !h_set && !document.body.classList.contains('prog')) {
-		var store = storage_get('prog');
-		if (store) for (var k in store) {
-			if (filters.hasOwnProperty(k)) filters[k] = store[k];
-		}
-	}
-	return filters;
-}
-
-function _prog_hash(f0, fx) {
-	var f = {}; for (var k in f0) f[k] = f0[k];
-	if (fx)     for (var k in fx) f[k] = fx[k];
-	var p = ['#prog'];
-	for (var k in f) if (k && f[k]) {
-		if ((k == 'area') && (f[k] == 'all_areas')) continue;
-		if (k == 'tag') {
-			if (f[k] == 'all_tags') continue;
-			if (ko.tag_categories && (f[k].indexOf(':') !== -1)) {
-				var s = f[k].split(':');
-				for (var j = 0; j < ko.tag_categories.length; ++j) {
-					if (s[0] == ko.tag_categories[j]) {
-						k = s[0];
-						f[k] = s[1];
-						break;
-					}
-				}
-			}
-		}
-		p.push(k + ':' + hash_encode(f[k]));
-	}
-	return p.length > 1 ? p.join('/') : '#';
-}
-
-function _prog_set_filters(f, silent) {
-	if (silent && !(history && history.replaceState)) return false;
-	storage_set('prog', f);
-	var h = _prog_hash(f),
-	    h_cur = window.location.toString().split('#')[1] || '';
-	if (h_cur != h.substr(1)) {
-		if (silent) {
-			var loc = window.location.toString().split('#')[0] + h;
-			history.replaceState({}, document.title, loc);
-		} else {
-			window.location.hash = h;
-		}
-		return true;
-	}
-	return false;
-}
-
-
-// hashchange -> read filters from url + store -> set filters in html + store -> list items
-function show_prog_view() {
-	function _show_filters(f) {
-		var prev = EL('prog_filters').getElementsByClassName('selected');
-		if (prev) for (var i = prev.length - 1; i >= 0; --i) {
-			var cl = prev[i].classList;
-			if (cl.contains('popup-link')) prev[i].textContent = prev[i].getAttribute('data-title') || 'More...';
-			cl.remove('selected');
-		}
-		for (var k in f) {
-			if (k == 'query') {
-				var q = EL("q");
-				if (q) {
-					q.value = f.query;
-					if (f.query) q.classList.add('selected');
-				}
-			} else {
-				var id = f[k];
-				if (!id) id = 'all_' + k + 's';
-				else if (id.match(/^[^a-zA-Z]/)) id = k[0] + id;
-				var el = EL(id);
-				if (el) {
-					el.classList.add('selected');
-					if (el.parentNode.id.match(/\d+$/)) {
-						var p = el.parentNode.parentNode.firstChild;
-						p.classList.add('selected');
-						p.textContent = el.textContent;
-					}
-				}
-			}
-		}
-		var qh = EL('q_hint');
-		if (qh) {
-			if (f.query) {
-				if (/[?*"]/.test(f.query)) {
-					qh.innerHTML = i18n_txt('search_hint');
-					qh.removeAttribute('onmouseup');
-					qh.style.cursor = 'auto';
-				} else {
-					qh.innerHTML = i18n_txt('search_hint') + ' ' + i18n_txt('search_example', {'X':f.query+'*'});
-					qh.onmouseup = function() { EL('q').value = f.query + '*'; EL('q').focus(); EL('q').blur(); };
-					qh.style.cursor = 'pointer';
-				}
-				qh.style.display = 'block';
-			} else {
-				qh.style.display = 'none';
-			}
-		}
-	}
-	function _filter(it) {
-		if (this.day && it.date != this.day) return false;
-		if (this.area) {
-			if (this.area instanceof RegExp) {
-				if (!this.area.test(it.loc.join(';'))) return false;
-			} else {
-				if (!it.loc || (it.loc.indexOf(this.area) < 0)) return false;
-			}
-		}
-		if (this.tag) {
-			if (this.tag instanceof RegExp) {
-				if (!this.tag.test(it.title)) return false;
-			} else {
-				if (!it.tags || (it.tags.indexOf(this.tag) < 0)) return false;
-			}
-		}
-		if (this.query) {
-			var found = this.query.test(it.title) || this.query.test(it.desc) || (it.loc && this.query.test(it.loc[0]));
-			if (!found && it.people) {
-				for (var i = 0; i < it.people.length; ++i) {
-					if (this.query.test(it.people[i].name)) { found = true; break; }
-				}
-			}
-			if (!found) return false;
-		}
-		return true;
-	}
-	function _show_list(f) {
-		var area_str = f.area || '';
-		if (f.area && EL(f.area)) {
-			var t = EL(f.area).getAttribute("data-regexp");
-			if (t) f.area = new RegExp(t);
-		}
-		var tag_str = f.tag || '';
-		if (f.tag && EL(f.tag)) {
-			var t = EL(f.tag).getAttribute("data-regexp");
-			if (t) f.tag = new RegExp(t);
-		}
-		var query_str = f.query || '';
-		if (f.query) f.query = GlobToRE(f.query);
-		var id_only = !!f.id;
-		if (id_only) for (var i in f) if ((i != 'id') && f[i]) {
-			id_only = false;
-			break;
-		}
-		var ls = program.filter(id_only ? function(it){ return it.id == f.id; } : _filter, f);
-		if (f.id) {
-			var id_ok = false;
-			for (var i = 0, l = ls.length; i < l; ++i) if (ls[i].id == f.id) {
-				id_ok = true;
-				break;
-			}
-			if (!id_ok) {
-				f.id = '';
-				if (_prog_set_filters(f)) return;
-			}
-		}
-		if (f.area) f.area = area_str;
-		if (f.tag) f.tag = tag_str;
-		if (f.query) f.query = query_str;
-		var fs = EL('filter_sum');
-		if (fs) {
-			var f0 = {};
-			for (var k in f) f0[k] = f[k];
-			f0['id'] = '';
-			if (!f0['day']) f0['day'] = 'all_days';
-			var _a = function(t, f0, fx) { return '<a href="' + _prog_hash(f0, fx) + '">' + t + '</a>'; }
-			if (id_only) {
-				fs.innerHTML = i18n_txt('filter_sum_id', { 'N':ls.length, 'TITLE':_a(ls[0].title, f0, {}), 'ID':_a(f.id, f0, {}) });
-			} else {
-				var d = { 'N':ls.length,
-					'ALL': f.tag || f.day || f.area || f.query ? '' : _a(i18n_txt('all'), {}, 0),
-					'TAG': f.tag ? _a(f.tag, f0, {'tag':''}) : '' };
-				if (f.area) { d['GOT_AREA'] = true; d['AREA'] = _a(f.area, f0, {'area':''}); }
-				if (f.query) {   d['GOT_Q'] = true;    d['Q'] = _a(f.query, f0, {'query':''}); }
-				if (f.day) {   d['GOT_DAY'] = true;  d['DAY'] = _a(i18n_txt('weekday_n', {'N': f.day ? parse_date(f.day).getDay() : -1 }), f0, {'day':'all_days'}); }
-				fs.innerHTML = i18n_txt('filter_sum', d);
-			}
-		}
-		item_show_list(ls, f.id);
-	}
-
-	var f = _prog_get_filters();
-	set_view("prog");
-	if (_prog_set_filters(f)) return;
-	if (!f.day && !f.id && !ko.show_all_days_by_default) f.day = _prog_default_day();
-	_show_filters(f);
-	for (var k in f) {
-		if (!k || !f[k] || (f[k] == 'all_' + k + 's')) { delete f[k]; continue; }
-	}
-	_show_list(f);
-}
-
-
-// read filters from url + ev -> set new hash
-function prog_filter_change(ev) {
-	ev = ev || window.event;
-
-	var key, value;
-	switch (ev.type) {
-		case 'click':
-			if (ev.target.tagName.toLowerCase() != 'li') return;
-			var popups = ev.target.getElementsByClassName('popup');
-			if (popups && popups.length) return;
-			key = ev.target.parentNode.id.replace(/\d+$/, '');
-			value = ev.target.id;
-			switch (key) {
-				case 'day':  value = value.replace(/^d/, ''); break;
-				case 'area': value = value.replace(/^a([^a-zA-Z])/, '$1'); break;
-			}
-			break;
-
-		case 'submit':
-			ev.preventDefault();
-			// fallthrough
-		case 'blur':
-			key = 'query';
-			value = EL("q").value;
-			break;
-
-		default: return;
-	}
-
-	var filters = _prog_get_filters();
-	filters[key] = value;
-	if (filters['id'] && (key != 'id')) filters['id'] = '';
-	_prog_set_filters(filters);
-}
-
-
 
 // ------------------------------------------------------------------------------------------------ participant view
 
@@ -909,21 +642,7 @@ EL('next_filters').onclick = next_filter_click;
 
 
 // init prog view
-var pf = EL('prog_filters');
-pf.onclick = prog_filter_change;
-
-var pl = pf.getElementsByClassName('popup-link');
-for (var i = 0; i < pl.length; ++i){
-	pl[i].setAttribute('data-title', pl[i].textContent);
-	pl[i].nextElementSibling.onclick = prog_filter_change;
-}
-
-var sf = EL('search');
-if (sf) {
-	sf.onsubmit = EL('q').onblur = prog_filter_change;
-	sf.onreset = function() { _prog_set_filters({}); };
-}
-
+var prog = new Prog();
 
 // init part view
 part_init();
@@ -985,7 +704,7 @@ function init_view() {
 		case 'star': show_star_view(opt.substr(4)); break;
 		case 'part': show_part_view(opt.substr(4)); break;
 		case 'info': show_info_view(); break;
-		default:     show_prog_view(); break;
+		default:     prog.show(); break;
 	}
 
 	if (EL("load_disable")) EL("load_disable").style.display = "none";
